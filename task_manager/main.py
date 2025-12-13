27e46a40d3afffff6da6befa79d97d6b72b39af3
Leonardo Ribeiro <Leonardo Ribeiro@LEO>
2025-12-13 04:24:00 -0300

    Dia 4: Editar e remover tarefas

--- task_manager/main.py
+++ task_manager/main.py
@@ -33,7 +33,9 @@ def show_menu():
     console.print("[1] Listar tarefas")
     console.print("[2] Adicionar tarefa") 
     console.print("[3] Concluir tarefa")
-    console.print("[4] Sair", style= "red")
+    console.print("[4] Editar tarefa")
+    console.print("[5] Remover tarefa")
+    console.print("[6] Sair", style= "red")
 
 def list_tasks():
     if not tasks:
@@ -43,19 +45,42 @@ def list_tasks():
     
     table.add_column("ID", style="cyan", no_wrap=True)
     table.add_column("Título", style="magenta")
-    table.add_column("Status", style="green", justify="center")    
+    table.add_column("Status", style="green", justify="center")  
     
     for task in tasks:
-            status = "✅ Concluída" if task["done"] else "⏳ Pendente"
-            table.add_row(str(task["id"]), task["title"], status)
+        status = "✅ Concluída" if task["done"] else "⏳ Pendente"
+        table.add_row(str(task["id"]), task["title"], status)
             
     console.print(table)
+    
+def edit_task():
+    list_tasks()
+    task_id = Prompt.ask("Digite o ID da tarefa que deseja editar")
+    for task in tasks:
+        if str(task["id"]) == task_id:
+            novo_titulo = Prompt.ask("Novo título da tarefa", default=task["title"])
+            task["title"] = novo_titulo
+            save_tasks()
+            console.print("[bold green]✏️  Tarefa atualizada com sucesso! [/bold green]")
+            return
+    console.print("[bold red]❌ ID não encontrado.[/bold red] ")
+        
+def remove_task():
+    list_tasks()
+    task_id = Prompt.ask("Qual tarefa(ID) deseja remover?")
+    for index, task in enumerate(tasks):
+        if str(task["id"]) == task_id:
+            tasks.pop(index)
+            save_tasks()
+            console.print("[bold green]🗑️  Tarefa removida![/bold green]")
+            return
+    console.print("[bold red]❌ Tarefa não encontrada")
 
 # Programa principal
 show_banner()
 while True:
     show_menu()
-    choice = IntPrompt.ask("Escolha", choices=["1", "2", "3", "4"])
+    choice = IntPrompt.ask("Escolha", choices=["1", "2", "3", "4", "5", "6"])
     
     if choice == 1:
         list_tasks()
@@ -77,6 +102,12 @@ while True:
             else:
                 console.print("[bold red]❌ ID não encontrado.[/bold red]")
     elif choice == 4:
+        edit_task()
+        
+    elif choice == 5:
+        remove_task()
+    
+    elif choice == 6:
         console.print("[bold blue]👋 Tchau![/]")
         break
     
